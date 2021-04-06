9f3c9ab2255efed416efdebd74226c82c4a07f3f
engeir <eirroleng@gmail.com>
2021-04-06 13:08:13 +0200

    Format with black

--- src/cesm_helper_scripts/gen_temp.py
+++ src/cesm_helper_scripts/gen_temp.py
@@ -1,4 +1,4 @@
-#! /cluster/home/een023/.virtualenvs/p3/bin/python 
+#! /cluster/home/een023/.virtualenvs/p3/bin/python
 """Send in a path to .nc files and the name of the files.
 Either as a list of many files or with the asterisk (wildcard?) notation, `"*.nc"`.
 Note that the wildcard notation must be sent in as a string like the above example.
@@ -8,81 +8,100 @@ Usage:
     gen_temp.py -p look/here -i one.nc two.nc -sp input -o output_name
 """
 
+import argparse
+import datetime
 import glob
 import os
 import sys
-import datetime
-import argparse
+
 import xarray as xr
-import numpy as np
 
 parser = argparse.ArgumentParser(
-    description='Create a file containing only the temperature variable.')
-parser.add_argument('-p', '--path', help='path to .nc files')
-parser.add_argument('-sp', '--savepath',
-                    help='path to where the .nc file is saved')
-parser.add_argument('-i', '--input', type=str, nargs='+',
-                    help='input .nc files. Use quotes around *, e.g. "*.nc".')
-parser.add_argument('-o', '--output', help='output .nc files')
-parser.add_argument('-y', '--year', action='store_true', help='Compute a 12 month running average.')
+    description="Create a file containing only the temperature variable."
+)
+parser.add_argument("-p", "--path", help="path to .nc files")
+parser.add_argument("-sp", "--savepath", help="path to where the .nc file is saved")
+parser.add_argument(
+    "-i",
+    "--input",
+    type=str,
+    nargs="+",
+    help='input .nc files. Use quotes around *, e.g. "*.nc".',
+)
+parser.add_argument("-o", "--output", help="output .nc files")
+parser.add_argument(
+    "-y", "--year", action="store_true", help="Compute a 12 month running average."
+)
 
 args = parser.parse_args()
 # Correct the input argument
 if args.input is None:
-    raise ValueError('you must give the input files')
+    raise ValueError("you must give the input files")
 # Correct the output argument
 if args.output is None:
     output = datetime.date.today().strftime("%Y%m%d")
 else:
     output = args.output
-if output.split('.')[-1] != 'nc':
-    output += '.nc'
+if output.split(".")[-1] != "nc":
+    output += ".nc"
 # Correct the path argument
 if args.path is not None:
-    path = args.path + '/' if args.path[-1] != '/' else args.path
+    path = args.path + "/" if args.path[-1] != "/" else args.path
 else:
-    path = ''
+    path = ""
 # Combine the path with all files
-inputs = [f'{path}{file}' if file.split(
-    '.')[-1] == 'nc' or '*' in file else f'{path}{file}.nc' for file in args.input]
+inputs = [
+    f"{path}{file}"
+    if file.split(".")[-1] == "nc" or "*" in file
+    else f"{path}{file}.nc"
+    for file in args.input
+]
 # If an asterisk (*) is used, all other files are discarded
-for f in inputs:
-    if '*' in f:
-        inputs = f
-        break
-if not glob.glob(inputs):
-    print(f'I could not find {inputs}')
-    print('Exiting...')
-    sys.exit()
+if any("*" in f for f in inputs):
+    val = inputs[[i for i, s in enumerate(inputs) if "*" in s][0]]
+    the_input = [val]
+    # if not glob.glob(the_input):
+    #     print(f"I could not find {the_input}")
+    #     print("Exiting...")
+    #     sys.exit()
+else:
+    the_input = inputs
+for input_ in the_input:
+    if not glob.glob(input_):
+        print(f"I could not find {input_}")
+        print("Exiting...")
+        sys.exit()
 # Correct the savepath argument
-savepath = args.savepath if args.savepath is not None else ''
-savepath = path if savepath == 'input' else savepath
-savepath = savepath + \
-    '/' if savepath != '' and savepath[-1] != '/' else savepath
+savepath = args.savepath if args.savepath is not None else ""
+savepath = path if savepath == "input" else savepath
+savepath = savepath + "/" if savepath != "" and savepath[-1] != "/" else savepath
 # Check if output file exist
 if os.path.exists(savepath + output):
-    ans = str(input(
-        f'The file {output} already exist in {savepath[:-1] if savepath != "" else "this directory"}. Do you want to overwrite this? (y/n)\t'))
-    if ans != 'y':
-        print('Exiting...')
+    ans = str(
+        input(
+            f'The file {output} already exist in {savepath[:-1] if savepath != "" else "this directory"}. Do you want to overwrite this? (y/n)\t'
+        )
+    )
+    if ans != "y":
+        print("Exiting...")
         sys.exit()
     else:
-        print('Saving to', savepath + output)
+        print("Saving to", savepath + output)
 else:
-    ans = str(input(f'Save to {savepath}{output}? (y/n)\t'))
-    if ans != 'y':
-        print('Exiting...')
+    ans = str(input(f"Save to {savepath}{output}? (y/n)\t"))
+    if ans != "y":
+        print("Exiting...")
         sys.exit()
     else:
-        if savepath != '':
+        if savepath != "":
             os.makedirs(savepath, exist_ok=True)
-        print('Saving to', savepath + output)
+        print("Saving to", savepath + output)
 
-dataset = xr.open_mfdataset(inputs)
+dataset = xr.open_mfdataset(the_input)
 dataset = xr.decode_cf(dataset)
 ds = dataset.T
 if args.year:
-    ds = ds.chunk({'time': 12})
+    ds = ds.chunk({"time": 12})
     r = ds.rolling(time=12)
     ds = r.mean()
 ds.to_netcdf(savepath + output)
--- src/cesm_helper_scripts/temp_plots.py
+++ src/cesm_helper_scripts/temp_plots.py
@@ -7,42 +7,56 @@ Usage:
     temp_plots -i single_file.nc -p up/three/layers -sp save_two_layers_below_input -o output_name -plt simpel sphere anim
 """
 
+import argparse
+import datetime
 import glob
 import os
 import sys
-import datetime
-import argparse
-import xarray as xr
-import numpy as np
-import nc_time_axis
-import matplotlib.pyplot as plt
-import matplotlib.animation as animation
+
 import cartopy.crs as ccrs
 import cartopy.feature as cfeat
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+import numpy as np
+import xarray as xr
 
 parser = argparse.ArgumentParser(
-    description='Create plots and animations wrt. temperature from a .nc file. \
+    description="Create plots and animations wrt. temperature from a .nc file. \
         Any number of plots can be generated: \
         (1) simpel: Temperature vs time \
         (2) sphere: Temperature vs (lat vs lon) at time `t` \
-        (3) anim:   Temperature vs (lat vs lon) animation.')
-parser.add_argument('-p', '--path', help='Relative path to .nc file. If not given, the current directory is used.')
-parser.add_argument('-sp', '--savepath',
-                    help='relative path to where the plot files are saved. If the savepath is -sp input, the same path is used here as is for the path parameter. If not given, the current directory is used.')
-parser.add_argument('-i', '--input', type=str,
-                    help='Input .nc file.')
-parser.add_argument('-o', '--output', help='Name of the output files.')
-parser.add_argument('-plt', '--plots', type=str, nargs='+',
-                    help='List of the plots that should be generated.')
+        (3) anim:   Temperature vs (lat vs lon) animation."
+)
+parser.add_argument(
+    "-p",
+    "--path",
+    help="Relative path to .nc file. If not given, the current directory is used.",
+)
+parser.add_argument(
+    "-sp",
+    "--savepath",
+    help="relative path to where the plot files are saved. If the savepath is -sp input, the same path is used here as is for the path parameter. If not given, the current directory is used.",
+)
+parser.add_argument("-i", "--input", type=str, help="Input .nc file.")
+parser.add_argument("-o", "--output", help="Name of the output files.")
+parser.add_argument(
+    "-plt",
+    "--plots",
+    type=str,
+    nargs="+",
+    help="List of the plots that should be generated.",
+)
 
 args = parser.parse_args()
 # Correct the input argument
 if args.input is None:
-    raise ValueError('you must give the input files')
+    raise ValueError("you must give the input files")
 if args.plots is None:
-    raise ValueError('you must specify what kind of plot you want (simple, sphere, anim)')
-if not set(args.plots).issubset(set(['simple', 'sphere', 'anim'])):
-    raise ValueError('you must choose between: simple, sphere, anim')
+    raise ValueError(
+        "you must specify what kind of plot you want (simple, sphere, anim)"
+    )
+if not set(args.plots).issubset({"simple", "sphere", "anim"}):
+    raise ValueError("you must choose between: simple, sphere, anim")
 # Correct the output argument
 if args.output is None:
     output = datetime.date.today().strftime("%Y%m%d")
@@ -50,59 +64,70 @@ else:
     output = args.output
 # Correct the path argument
 if args.path is not None:
-    path = args.path + '/' if args.path[-1] != '/' else args.path
+    path = args.path + "/" if args.path[-1] != "/" else args.path
 else:
-    path = ''
+    path = ""
 # Combine the path with all files
-inputs = f'{path}{args.input}' if args.input.split(
-    '.')[-1] == 'nc' else f'{path}{args.input}.nc'
+inputs = (
+    f"{path}{args.input}"
+    if args.input.split(".")[-1] == "nc"
+    else f"{path}{args.input}.nc"
+)
 if not glob.glob(inputs):
-    print(f'I could not find {inputs}')
-    print('Exiting...')
+    print(f"I could not find {inputs}")
+    print("Exiting...")
     sys.exit()
 # Correct the savepath argument
-savepath = args.savepath if args.savepath is not None else ''
-savepath = path if savepath == 'input' else savepath
-savepath = savepath + \
-    '/' if savepath != '' and savepath[-1] != '/' else savepath
+savepath = args.savepath if args.savepath is not None else ""
+savepath = path if savepath == "input" else savepath
+savepath = savepath + "/" if savepath != "" and savepath[-1] != "/" else savepath
+
 # Check if output file exist
+
+
 def file_exist(end):
     if os.path.exists(savepath + output + end):
-        ans = str(input(
-            f'The file {output}{end} already exist in {savepath[:-1] if savepath != "" else "this directory"}. Do you want to overwrite this? (y/n)\t'))
-        if ans != 'y':
-            print('Exiting without making any plots...')
+        ans = str(
+            input(
+                f'The file {output}{end} already exist in {savepath[:-1] if savepath != "" else "this directory"}. Do you want to overwrite this? (y/n)\t'
+            )
+        )
+        if ans != "y":
+            print("Exiting without making any plots...")
             sys.exit()
         else:
-            print('Saving to', savepath + output + end)
+            print("Saving to", savepath + output + end)
     else:
-        ans = str(input(f'Save to {savepath}{output}{end}? (y/n)\t'))
-        if ans != 'y':
-            print('Exiting without making any plots...')
+        ans = str(input(f"Save to {savepath}{output}{end}? (y/n)\t"))
+        if ans != "y":
+            print("Exiting without making any plots...")
             sys.exit()
         else:
-            if savepath != '':
+            if savepath != "":
                 os.makedirs(savepath, exist_ok=True)
-            print('Saving to', savepath + output + end)
+            print("Saving to", savepath + output + end)
 
 
-if 'simple' in args.plots:
-    file_exist('_temp.png')
-if 'sphere' in args.plots:
-    file_exist('_sphere.png')
-if 'anim' in args.plots:
-    file_exist('.mp4')
+if "simple" in args.plots:
+    file_exist("_temp.png")
+if "sphere" in args.plots:
+    file_exist("_sphere.png")
+if "anim" in args.plots:
+    file_exist(".mp4")
 
 # === <CODE> ===
+
+
 def spherical_plot(time_temp):
     # projections: https://scitools.org.uk/cartopy/docs/latest/crs/projections.html
     # PlateCarree(), Orthographic(-80, 35)
     p = time_temp.isel(time=0).plot(
         subplot_kws=dict(projection=ccrs.Mollweide(), facecolor="gray"),
-        transform=ccrs.PlateCarree(),)
+        transform=ccrs.PlateCarree(),
+    )
     p.axes.set_global()
     p.axes.coastlines()
-    plt.savefig(f'{savepath}{output}_sphere.png')
+    plt.savefig(f"{savepath}{output}_sphere.png")
     plt.close()
 
 
@@ -115,13 +140,18 @@ def temperature_animation(time_temp):
         # ax.add_feature(cfeat.LAND)
         # ax.add_feature(cfeat.OCEAN)
         ax.add_feature(cfeat.COASTLINE)
-        ax.add_feature(cfeat.BORDERS, linestyle='dotted')
+        ax.add_feature(cfeat.BORDERS, linestyle="dotted")
         return fig, ax
 
     def draw(frame, add_colorbar):
         grid = time_temp[frame]
-        contour = grid.plot(ax=ax, transform=ccrs.PlateCarree(),
-                            add_colorbar=add_colorbar, vmin=min_value, vmax=max_value)
+        contour = grid.plot(
+            ax=ax,
+            transform=ccrs.PlateCarree(),
+            add_colorbar=add_colorbar,
+            vmin=min_value,
+            vmax=max_value,
+        )
         # title = u"%s — %s" % (ds.t2m.long_name, str(
         #     time_temp.time[frame].values)[:19])
         # ax.set_title(title)
@@ -135,14 +165,14 @@ def temperature_animation(time_temp):
 
     fig, ax = make_figure()
 
-    frames = time_temp.time.size        # Number of frames
+    frames = time_temp.time.size  # Number of frames
     min_value = time_temp.values.min()  # Lowest value
     max_value = time_temp.values.max()  # Highest value
 
-    ani = animation.FuncAnimation(fig, animate, frames, interval=0.01, blit=False,
-                                  init_func=init, repeat=False)
-    ani.save(f'{savepath}{output}.mp4',
-             writer=animation.FFMpegWriter(fps=8))
+    ani = animation.FuncAnimation(
+        fig, animate, frames, interval=0.01, blit=False, init_func=init, repeat=False
+    )
+    ani.save(f"{savepath}{output}.mp4", writer=animation.FFMpegWriter(fps=8))
     plt.close(fig)
 
 
@@ -151,21 +181,23 @@ def just_temp(temps):
     # https://xarray.pydata.org/en/stable/examples/area_weighted_temperature.html
     # Need mean = ( sum n*cos(lat) ) / ( sum cos(lat) )
     weights = np.cos(np.deg2rad(temps.lat))
-    weights.name = 'weights'
+    weights.name = "weights"
     air_weighted = temps.weighted(weights)
-    k_w = air_weighted.mean(('lon', 'lat'))
+    k_w = air_weighted.mean(("lon", "lat"))
     # k = temps.mean(dim=['lat', 'lon'])
     k_w.plot()
-    plt.savefig(f'{savepath}{output}_temp.png')
+    plt.savefig(f"{savepath}{output}_temp.png")
     plt.close()
+
+
 # === </CODE> ===
 
 
 multi_T = xr.open_dataarray(inputs)
 multi_T = multi_T.isel(lev=0)
-if 'simple' in args.plots:
+if "simple" in args.plots:
     just_temp(multi_T)
-if 'sphere' in args.plots:
+if "sphere" in args.plots:
     spherical_plot(multi_T)
-if 'anim' in args.plots:
+if "anim" in args.plots:
     temperature_animation(multi_T)
